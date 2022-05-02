63a6dfcbbacc9578408ec976c9e0e036458683f9
lucasvm8 <lucasvm8@gmail.com>
2022-05-02 17:15:27 -0300

    feat: propTypes & defaultProps

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,21 @@
-function Item (props) {
+import PropTypes from 'prop-types'
+
+function Item ({ marca, lancamento }) {
     return (
-        <li>{props.marca}</li>
+        <li>
+          {marca} - {lancamento}
+        </li>
     )
+}
+
+Item.propTyepes = {
+    marca: PropTypes.string,
+    lancamento: PropTypes.number
+}
+
+Item.defaultProps = {
+    marca: 'Sem marca',
+    lancamento: 0
 }
 
 export default Item
--- src/components/List.js
+++ src/components/List.js
@@ -5,8 +5,9 @@ function List () {
       <>
           <h1>Minha Lista</h1>
           <ul>
-              <Item marca="Ferrari" />
-              <Item marca="Lambo" />
+              <Item marca="Ferrari" lancamento={1990} />
+              <Item marca="Lambo" lancamento={2000}/>
+              <Item />
           </ul>
       </>
     )
